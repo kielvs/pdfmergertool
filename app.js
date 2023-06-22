1d773e843b9446bc55dd6cc3427ce2642be4ce15
kielvs <128386154+kielvs@users.noreply.github.com>
2023-06-22 09:36:52 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// app.js
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const port = 3005;
+
+app.listen(port, () => {
+    console.log('Server is running on',port);
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,74 @@
+// routes/index.js
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const fs = require('fs');
+const { PDFDocument } = require('pdf-lib');
+const axios = require('axios');
+
+const upload = multer({ dest: 'uploads/' });
+
+function formatTimestamp() {
+  const date = new Date();
+  const options = {
+    month: '2-digit',
+    day: '2-digit',
+    year: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  };
+  const formattedDate = date.toLocaleString('en-US', options).replace(/[^\w]/g, '-');
+  return formattedDate;
+}
+router.post('/merge-pdf', upload.none(), async (req, res) => {
+  const { url1, url2 } = req.body;
+
+  try {
+    // Download the first PDF
+    const firstPdfBuffer = await downloadPdf(url1);
+
+    // Download the second PDF
+    const secondPdfBuffer = await downloadPdf(url2);
+
+    // Load the first and second PDFs
+    const firstPdf = await PDFDocument.load(firstPdfBuffer);
+    const secondPdf = await PDFDocument.load(secondPdfBuffer);
+
+    // Create a new PDF document
+    const mergedPdf = await PDFDocument.create();
+
+    // Copy pages from the first PDF to the merged document
+    const firstPdfPages = await mergedPdf.copyPages(firstPdf, firstPdf.getPageIndices());
+    firstPdfPages.forEach((page) => mergedPdf.addPage(page));
+
+    // Copy pages from the second PDF to the merged document
+    const secondPdfPages = await mergedPdf.copyPages(secondPdf, secondPdf.getPageIndices());
+    secondPdfPages.forEach((page) => mergedPdf.addPage(page));
+
+    // Save the merged PDF to a file
+    const mergedPdfBytes = await mergedPdf.save();
+    const timestamp = formatTimestamp();
+    const mergedPdfPath = `uploads/merged_${timestamp}.pdf`;
+    
+
+    fs.writeFileSync(mergedPdfPath, mergedPdfBytes);
+
+    res.status(200).json({ message: 'PDFs merged successfully', mergedPdfPath });
+  } catch (error) {
+    console.error('Error merging PDFs:', error);
+    res.status(500).json({ error: 'An error occurred while merging PDFs' });
+  }
+});
+
+async function downloadPdf(url) {
+  try {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = router;
